9e74e7ba67739248cd92666373e93ba5066fa5e1
ChoZeur <chozeur@protonmail.com>
2023-01-17 21:05:03 +0100

    --requirements -gitignore(.pypirc)

--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ setup(
 	name='openFrom',
 	version='0.1',
 	py_modules=['openFrom'],
-	install_requires=[
-		'argparse',
-		'subprocess'
-	],
 	entry_points={
 		'console_scripts': [
 			'openFrom=openFrom:main'
